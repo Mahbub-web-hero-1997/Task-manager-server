bb86cb915208aa1c82f91066d9caed7f1cd2ddf0
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-05-26 16:55:04 +0600

    user register function added

---
+++ src/Controllers/User.controller.js
@@ -0,0 +1,41 @@
+import User from "../Models/user.models";
+import ApiError from "../Utils/ApiError";
+import ApiResponse from "../Utils/ApiResponse";
+
+const registerUser = async (req, res) => {
+  const { fullName, email, password, confirmPassword } = req.body;
+  //   Body Data Validation
+  if ((!fullName, !email, !password, !confirmPassword)) {
+    throw new ApiError(400, "All fields are required");
+  }
+  if (
+    [fullName, email, password, confirmPassword].some(
+      (field) => field.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields must have some value");
+  }
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new ApiError(400, "Email already exists");
+  }
+  if (password !== confirmPassword) {
+    throw new ApiError(400, "Password do not match");
+  }
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+  });
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Failed to register User");
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(200, createdUser, "User successfully registered"));
+};
+
+export { registerUser };
